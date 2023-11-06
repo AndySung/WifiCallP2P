9e216d18bdaeb350798ace9173f981cd48f37192
andysong <songzongjian0123@163.com>
2023-11-06 17:11:01 +0800

    添加設置界面

--- app/src/main/java/com/example/tablayout/Fragments/RecentCallFragment.java
+++ app/src/main/java/com/example/tablayout/Fragments/RecentCallFragment.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.tablayout.R;
---
+++ app/src/main/java/com/example/tablayout/ui/activity/SettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.tablayout.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SettingActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
